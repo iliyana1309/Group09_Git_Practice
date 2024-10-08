6e59fd75bf0145db1fb1686a905836b84ba121e5
tetianakhoptii <tkhoptii@gmail.com>
2024-10-08 14:00:18 -0400

    Update from Tanya in Julia's file

--- src/Julia_practice/Julia_class.java
+++ src/Julia_practice/Julia_class.java
@@ -3,5 +3,8 @@ package Julia_practice;
 public class Julia_class {
     public static void main(String[] args) {
         // blabla Hello!
+
+        System.out.println("Hello Julia from Tanya");
+        System.out.println("Please let me know if you see this message");
     }
 }
